100266c0151531d46a1cf36459335fba87795ccf
Atharva Tikhe <atharva.tikhe@medgenome.com>
2024-12-23 12:22:18 +0530

    fix: updated config with Mason

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -19,6 +20,17 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- import your plugins
+		{ 'williamboman/mason.nvim',
+				
+				config = function()
+						require('mason').setup()
+				end
+		},
+		{ 'williamboman/mason-lspconfig.nvim', 
+				config = function()
+						require('mason-lspconfig').setup()
+				end
+		},
     { import = "config.plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
