314b1e3056a9bd60d3a01ff2dc0c43d4f0a20e4b
AtharvaTikhe <atharvatikhe22@gmail.com>
2025-01-19 21:09:01 +0530

    feat: added lualine (not working)

---
+++ lua/config/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+		enabled = false,
+		config = function()
+				require('lualine').setup {
+
+						options = {
+								icons_enabled = false,
+								theme = 'auto',
+								refresh = {
+										statusline = 50,
+										tabline = 100,
+										winbar = 100,
+								},
+								  sections = {
+										lualine_a = {'mode'},
+										lualine_b = {'branch', 'diff', 'diagnostics'},
+										lualine_c = {'filename'},
+										lualine_x = {'encoding', 'fileformat', 'filetype'},
+										lualine_y = {'progress'},
+										lualine_z = {'location'}
+							},
+						}
+				}
+		end
+}
