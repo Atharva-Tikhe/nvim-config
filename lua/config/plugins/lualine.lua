23cf9acfc9a21ede9f07044192fbf76c07a88856
AtharvaTikhe <atharvatikhe22@gmail.com>
2025-01-24 14:34:41 +0530

    fix: lualine working

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -1,27 +1,35 @@
+
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-		enabled = false,
 		config = function()
+				local function hello()
+						return [[ HII ]]
+				end
 				require('lualine').setup {
+				options = {
+						icons_enabled = false,
+						theme = 'onedark',
+						refresh = {
+								statusline = 50,
+								tabline = 100,
+								winbar = 100,
+						},
+						component_separators = {left = "|", right = "::"},
+						section_separators = {left = "|", right = "::"},
+					sections = {
+							lualine_a = {hello},
+						-- lualine_a = {'mode'},
+						-- lualine_b = {'branch', 'diff', 'diagnostics'},
+						lualine_b = {},
+						lualine_c = {'filename'},
+						lualine_x = {'fileformat', 'filetype'},
+						lualine_y = {'progress'},
+						lualine_z = {'location'}
+			},
 
-						options = {
-								icons_enabled = false,
-								theme = 'auto',
-								refresh = {
-										statusline = 50,
-										tabline = 100,
-										winbar = 100,
-								},
-								  sections = {
-										lualine_a = {'mode'},
-										lualine_b = {'branch', 'diff', 'diagnostics'},
-										lualine_c = {'filename'},
-										lualine_x = {'encoding', 'fileformat', 'filetype'},
-										lualine_y = {'progress'},
-										lualine_z = {'location'}
-							},
-						}
 				}
+		}
 		end
 }
